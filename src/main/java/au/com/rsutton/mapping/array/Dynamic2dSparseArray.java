b7ca6c40359290acd5ca55f79e845f7bf4695c7a
Robert Sutton <rsutton@noojee.com.au>
2017-01-13 17:35:56 +1100

    expose getter for the default unexplored value

--- src/main/java/au/com/rsutton/mapping/array/Dynamic2dSparseArray.java
+++ src/main/java/au/com/rsutton/mapping/array/Dynamic2dSparseArray.java
@@ -15,37 +15,44 @@ public class Dynamic2dSparseArray
 
 	private int minX = Integer.MAX_VALUE;
 
+	private final double defaultValue;
+
 	public Dynamic2dSparseArray(double defaultValue)
 	{
 		// +-(50^3) is the maximum array size
 		this.cellSize = 50;
 		int levels = 3;
-		
 
-		maps[0][0] = new Sparse2dSegment(cellSize,levels,defaultValue);
-		maps[0][1] = new Sparse2dSegment(cellSize,levels,defaultValue);
-		maps[1][0] = new Sparse2dSegment(cellSize,levels,defaultValue);
-		maps[1][1] = new Sparse2dSegment(cellSize,levels,defaultValue);
+		this.defaultValue = defaultValue;
 
+		maps[0][0] = new Sparse2dSegment(cellSize, levels, defaultValue);
+		maps[0][1] = new Sparse2dSegment(cellSize, levels, defaultValue);
+		maps[1][0] = new Sparse2dSegment(cellSize, levels, defaultValue);
+		maps[1][1] = new Sparse2dSegment(cellSize, levels, defaultValue);
 
 	}
-	
+
+	public double getDefaultValue()
+	{
+		return defaultValue;
+	}
+
 	public double get(int x, int y)
 	{
-		Sparse2dSegment map = maps[(int) Math.max(0,Math.signum(x))][(int) Math.max(0, Math.signum(y))];
-		return map.get(Math.abs(x),Math.abs(y));
+		Sparse2dSegment map = maps[(int) Math.max(0, Math.signum(x))][(int) Math.max(0, Math.signum(y))];
+		return map.get(Math.abs(x), Math.abs(y));
 	}
 
 	public void set(int x, int y, double value)
 	{
 		minX = Math.min(minX, x);
-		minY = Math.min(minY,y);
-		maxX = Math.max(maxX,x);
+		minY = Math.min(minY, y);
+		maxX = Math.max(maxX, x);
 		maxY = Math.max(maxY, y);
-		
-		Sparse2dSegment map = maps[(int) Math.max(0,Math.signum(x))][(int) Math.max(0, Math.signum(y))];
-		 map.set(Math.abs(x),Math.abs(y),value);
-		
+
+		Sparse2dSegment map = maps[(int) Math.max(0, Math.signum(x))][(int) Math.max(0, Math.signum(y))];
+		map.set(Math.abs(x), Math.abs(y), value);
+
 	}
 
 	public int getMinY()
