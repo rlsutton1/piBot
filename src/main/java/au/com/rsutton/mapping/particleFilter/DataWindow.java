e741ed539a3a50f4eee70f7b3961bbd7842449cc
Robert Sutton <rsutton@noojee.com.au>
2019-01-08 22:59:31 +1100

    make data window display correctly

--- src/main/java/au/com/rsutton/mapping/particleFilter/DataWindow.java
+++ src/main/java/au/com/rsutton/mapping/particleFilter/DataWindow.java
@@ -1,5 +1,7 @@
 package au.com.rsutton.mapping.particleFilter;
 
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 public class DataWindow extends JFrame
@@ -15,9 +17,14 @@ public class DataWindow extends JFrame
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		setSize(400, 400);
-		setLocation(200, 200);
+		setLocation(200, 600);
 		setVisible(true);
-		add(new DataPanel());
+		DataPanel dataPanel = new DataPanel();
+		add(dataPanel);
+
+		dataPanel.setPreferredSize(new Dimension(400, 400));
+		dataPanel.setVisible(true);
+		this.setBounds(200, 600, 410, 410);
 
 	}
 
